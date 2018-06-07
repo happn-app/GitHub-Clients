488264cd93ff08570ca9d8031b4d6794f52030fb
Frizlab <fload@me.com>
2018-06-07 12:59:44 +0200

    Initial Commit

---
+++ GitHubBridge/GitHubBMOBridge.swift
@@ -0,0 +1,17 @@
+/*
+ * GitHubBMOBridge.swift
+ * GitHub Clients
+ *
+ * Created by François Lamboley on 07/06/2018.
+ * Copyright © 2018 happn. All rights reserved.
+ */
+
+import Foundation
+
+
+
+public class GitHubBMOBridge {
+   
+   public func hello() -> String {return "hello"}
+   
+}
---
+++ GitHubBridgeTests/GitHubBridgeTests.swift
@@ -0,0 +1,20 @@
+/*
+ * GitHubBridgeTests.swift
+ * GitHubBridgeTests
+ *
+ * Created by François Lamboley on 07/06/2018.
+ * Copyright © 2018 happn. All rights reserved.
+ */
+
+import XCTest
+@testable import GitHubBridge
+
+
+
+class GitHubBridgeTests: XCTestCase {
+   
+   func testExample() {
+      XCTAssertEqual(GitHubBMOBridge().hello(), "hello")
+   }
+   
+}
