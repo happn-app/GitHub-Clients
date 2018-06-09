ff6ce97e229b63cf06195db68f10bbf080e836c6
Frizlab <fload@me.com>
2018-06-09 11:10:43 +0200

    Add iOS App

---
+++ GitHub iOS/FirstViewController.swift
@@ -0,0 +1,25 @@
+/*
+ * FirstViewController.swift
+ * GitHub iOS
+ *
+ * Created by François Lamboley on 08/06/2018.
+ * Copyright © 2018 happn. All rights reserved.
+ */
+
+import UIKit
+
+
+
+class FirstViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+}
