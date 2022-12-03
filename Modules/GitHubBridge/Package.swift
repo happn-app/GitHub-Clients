2b3671bb47f7c00612dbe3381be4bcb8315e9851
Frizlab <fload@me.com>
2022-12-03 01:15:38 +0100

    Add proper dependency to GitHubBridge

--- Modules/GitHubBridge/Package.swift
+++ Modules/GitHubBridge/Package.swift
@@ -7,10 +7,12 @@ let package = Package(
 	platforms: [.iOS(.v12)],
 	products: [.library(name: "GitHubBridge", targets: ["GitHubBridge"])],
 	dependencies: [
-		.package(url: "https://github.com/Frizlab/LinkHeaderParser.git", branch: "main")
+		.package(url: "https://github.com/Frizlab/LinkHeaderParser.git", branch: "main"),
+		.package(url: "https://github.com/happn-app/BMO.git", branch: "dev.paginator_enhancements")
 	],
 	targets: [
 		.target(name: "GitHubBridge", dependencies: [
+			.product(name: "Jake", package: "BMO"),
 			.product(name: "LinkHeaderParser", package: "LinkHeaderParser")
 		]/*, swiftSettings: [
 			.unsafeFlags(["-Xfrontend", "-warn-concurrency", "-Xfrontend", "-enable-actor-data-race-checks"])
