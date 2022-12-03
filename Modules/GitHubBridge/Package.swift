eabced6dbf0e2929ed5d3adb3334435a7fd0cecc
Frizlab <fload@me.com>
2022-12-03 01:50:42 +0100

    Use another version of BMO

--- Modules/GitHubBridge/Package.swift
+++ Modules/GitHubBridge/Package.swift
@@ -8,7 +8,7 @@ let package = Package(
 	products: [.library(name: "GitHubBridge", targets: ["GitHubBridge"])],
 	dependencies: [
 		.package(url: "https://github.com/Frizlab/LinkHeaderParser.git", branch: "main"),
-		.package(url: "https://github.com/happn-app/BMO.git", branch: "dev.paginator_enhancements")
+		.package(url: "https://github.com/happn-app/BMO.git", branch: "dev.tests")
 	],
 	targets: [
 		.target(name: "GitHubBridge", dependencies: [
